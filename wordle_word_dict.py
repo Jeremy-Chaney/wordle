4afc7c061980676d607cf4f90fe7176371c0f829
Jeremy-Chaney <69745430+Jeremy-Chaney@users.noreply.github.com>
2022-02-08 23:40:07 -0600

    Update wordle_word_dict.py
    updated for "ascii" easter-egg

--- wordle_word_dict.py
+++ wordle_word_dict.py
@@ -2,8 +2,7 @@ from platform import java_ver
 from re import I
 from this import d
 
-# list of possible answers in alphabetical order
-# these are also in the valid_word_list, but are all more common words
+
 valid_answer_list = [
 	'aback',
 	'abase',
@@ -2322,7 +2321,93 @@ valid_answer_list = [
 	'zonal'
 ]
 
-# list of all possible input words
+ascii_fail = [
+    "",
+    "",
+    "",
+    "                $$$$$",
+    "              $$     $$",
+    "              $$ xxx $$",
+    "              $$     $$",
+    "              $$ xxx $$",
+    "              $$     $$",
+    "              $$ xxx $$",
+    "          $$$$$$     $$$$$$",
+    "        $$    $$ xxx $$    $$$$",
+    "        $$    $$     $$    $$  $$",
+    "$$$$$$  $$    $$ xxx $$    $$    $$",
+    "$$    $$$$                 $$    $$",
+    "$$      $$                       $$",
+    "  $$    $$                       $$",
+    "   $$$  $$                       $$",
+    "    $$                           $$",
+    "     $$$                         $$",
+    "      $$                        $$$",
+    "       $$$                     $$",
+    "        $$                     $$",
+    "         $$$                  $$$",
+    "          $$                  $$",
+    "          $$$$$$$$$$$$$$$$$$$$$$"
+]
+
+ascii_pass = [
+    "                                       ,aaa,",
+    "                                     ,dP\"\'Y8,",
+    "                                    dP\"   `8b",
+    "                                   dI      I8",
+    "    ,ad88b,                       ,8\'      I8d88b,",
+    "   d8\"\' `\"8,                      fP xxx  ,8\"  `\"8,",
+    "   8I     `8,                     8\'      dP xxx I8",
+    "   8b xxx  `8,                   ,8       8\'     8I",
+    "   I8,      Ib,                  dP      ,8      8I",
+    " ad88b,     `Yb                 ,8\'      I8      8I",
+    "dP\' `8b,     `8,                dP       IP      8I",
+    "8\'   `8I      \"8,              ,8\'       8I      8\'",
+    "8     Yb,      \"8,            ,8\'        8\'      8",
+    "8 xxx `8I       Ib,           dP        ,8      ,8",
+    "8,     `8,      `8I          ,8\'        dP      I8",
+    "8I      YI       `8,         dP        ,8\'      8I",
+    "Ib      `8,       `8,       ,8\'        8P       8\'",
+    "I8       8I        Yb       dP        f8\'       8",
+    "`8,      I8,       `8,     dP\'        8I       ,8",
+    " 8I      `8I        Yb    dP\'        ,8\'       I8",
+    " Ib       `8,       `YbaadP\'         d8        8I",
+    " `8,       YI         `\"\"\'           8P        8\'",
+    "  Ib,      `8,                                 8",
+    "  `8I       YI                                ,8",
+    "   I8       `\"                                I8",
+    "   I8                                         I8",
+    "   I8                                         I8",
+    "   I8                                         I8                        ______",
+    "   `8                                         `8,                  _,add8888888bba,.",
+    "    8,                                         YI               ,ad8P\"\'         ``\"Yb",
+    "    8I                                         `8b,          ,ad8P\"\'      xxx      I8)",
+    "    8b                                           \"8ba,___,ad8P\"\'          ,aaaaaaad8P\'",
+    "    I8                                             `\"Y888P\"\'          ,adPP\"\"\"\"\"\"\"\'\'",
+    "    `8,                                                             ,dP\"\'",
+    "     8I                                                           ,8P\"",
+    "     8b                                                         ,8P\"",
+    "     I8                                                       ,8P\"",
+    "     Y8                                                     ,8P\"",
+    "     I8                                                   ,8P\"",
+    "     `8,                                                ,dP\"",
+    "      8I                                              a8P\"",
+    "      `8,                                          ,d8P\'",
+    "       `8,                                      ,adP\"",
+    "        `8,                                  ,adP\"\'",
+    "         8I                               ,adP\"",
+    "         I8                             ,dP\'",
+    "         8I                           ,8P\'",
+    "         8P                         ,dP\"",
+    "        ,8\'                        ,8P\'",
+    "        dI                        ,8P",
+    "       ,8\'                       ,8P",
+    "       IP                       ,8P",
+    "                                8P",
+    "                               dP",
+    "                              \"\""
+]
+
 valid_word_list = [
     'aahed',
 	'aalii',
@@ -15298,10 +15383,8 @@ valid_word_list = [
 	'zymic'
 ]
 
-# list of all lower-case letters used to check for keyboard updates
 all_letters = 'abcdefghijklmnopqrstuvwxyz'
 
-# used to find the index of each letter
 valid_char_list = [
     'a',
     'b',
@@ -15331,11 +15414,6 @@ valid_char_list = [
     'z'
 ]
 
-# stores the status of each letter
-# 0 -> white  -> no status
-# 1 -> yellow -> correct letter but in the wrong spot
-# 2 -> green  -> correct letter in correct spot
-# 3 -> grey   -> letter does not appear in the final word
 keyboard_status = [
     '0', #a
     '0', #b
